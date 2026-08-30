5fdc61e30519864e6ee96204cd915ba4da6c65db
Abdalle13 <zekovic258@gmail.com>
2026-08-30 12:59:04 +0300

    put the copyright line above the designer credit in the footer

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -90,6 +90,7 @@ const Footer = () => {
 
         {/* Bottom bar */}
         <div className="border-t border-line pt-6 flex flex-col items-center gap-1.5 text-center text-[12px] text-muted">
+          <p>© {year} Kobac Electronics. All rights reserved.</p>
           <p>
             Designed &amp; built by{' '}
             <a
@@ -101,7 +102,6 @@ const Footer = () => {
               Abdalle Hussein
             </a>
           </p>
-          <p>© {year} Kobac Electronics. All rights reserved.</p>
         </div>
       </div>
     </footer>
